bb9d7d3041a9734c226ad645b0be8c30097615be
ente <uiop11@posteo.de>
2017-11-14 18:08:21 +0100

    akdjsflkajsdflak

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -24,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-
-        display(quantity);
-        displayPrice(quantity*5);
-
+        int price = quantity* 5;
+        String priceMessage="total: $ " + price;
+        priceMessage = priceMessage + " wohoo";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -57,6 +57,15 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
